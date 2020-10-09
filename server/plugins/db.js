9e3a73b9a464e6a2c5b68745a1ff87f27855770b
Chenxi <chenxic1011@gmail.com>
2020-10-09 16:11:08 -0400

    Update db.js

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -3,7 +3,7 @@ module.exports = app => {
 
   try {
     mongoose.connect(
-      'mongodb+srv://brad123:brad123@bootcamp-suun9.mongodb.net/vue?retryWrites=true&w=majority',
+      'xxxxxxxxxxxxx',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
